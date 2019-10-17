084ebeb600ad8db6c3041e3eeaff28984422b8d9
Hrittik Das <hrittikdas16@gmail.com>
2019-10-17 10:00:09 +0530

    Update bfs.cpp

--- graph/bfs.cpp
+++ graph/bfs.cpp
@@ -55,8 +55,8 @@ int main() {
         g.addEdge(0, 4);
         g.addEdge(2, 4);
         g.addEdge(3, 2);
-		g.addEdge(2, 3);
-		g.addEdge(3, 5);
+	g.addEdge(2, 3);
+	g.addEdge(3, 5);
 
         g.bfs(0);
 
