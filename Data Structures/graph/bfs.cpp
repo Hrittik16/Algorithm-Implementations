872f55f573f52e64ef3f1af6b2fca3ffe22292ce
hrittik16 <hrittikdas16@gmail.com>
2019-12-05 16:58:04 +0530

    bfs updated

--- Data Structures/graph/bfs.cpp
+++ Data Structures/graph/bfs.cpp
@@ -1,65 +1,44 @@
-// BFS(Breadth First Search) - iterative way of traversing
 #include <bits/stdc++.h>
 using namespace std;
-class Graph {
-public:
-        int V;  // number of vertices
-        // Array of linked lists of size V
-        list<int> *adjList;
-		vector<int> dist;
-        Graph(int v) {
-                V = v;
-                adjList = new list<int>[V];
-		dist = vector<int>(V); // dist is the depth of ith node or we can say distance between the ith node and the root node.
-        }
-        void addEdge(int u, int v, bool bidir=true) {
-                adjList[u].push_back(v);
-                if(bidir) adjList[v].push_back(u);
-        }
-        void printAdjList() {
-                for(int i = 0; i < V; i++) {
-                        cout << i << " -> ";
-                        for(auto node: adjList[i]) {
-                                cout << node << ",";
-                        }
-                        cout << "\n";
-                }
-        }
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define rep(i, a, b) for(int i = a; i < b; i++) 
+#define repll(i, a, b) for(ll i = a; i < b; i++) 
+#define all(a) a.begin(),a.end()
+#define pb push_back
 
-	void bfs(int src) {
-		// traverse allthe nodes of the graph
-		queue<int> q;
-		bool *visited = new bool[V]{0};
-		q.push(src);
-		visited[src] = true;
-		dist[src] = 0;
-		while(!q.empty()) {
-			int node = q.front();
-			cout << node << ",";
-			q.pop();
-			for(int neighbour: adjList[node]) {
-				if(!visited[neighbour]) {
-					q.push(neighbour);
-					visited[neighbour] = true;
-					dist[neighbour] = dist[node]+1; 
-				}
+void bfs(vector<vector<int>>& list, int n, int beg) {
+	queue<int> q;
+	vector<bool> check(n+1, 0);
+	vector<int> dist(n+1, 0); // bfs gives the shortest number of edges to go from beg(source) to any other node
+	q.push(beg);
+	check[beg] = 1;
+	while(!q.empty()) {
+		int curr = q.front();
+		q.pop();
+		for(auto x: list[curr]) {
+			if(!check[x]) {
+				check[x] = 1;
+				q.push(x);
+				dist[x] = dist[curr]+1;
 			}
 		}
-		cout << "\n";
 	}
-};
-int main() {
-        Graph g(6);
-        g.addEdge(0, 1);
-        g.addEdge(1, 2);
-        g.addEdge(0, 4);
-        g.addEdge(2, 4);
-        g.addEdge(3, 2);
-	g.addEdge(2, 3);
-	g.addEdge(3, 5);
-
-        g.bfs(0);
-
-        return 0;
 }
 
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> list(n+1); // adj list
+	rep(i, 0, m) {
+		int x, y;
+		cin >> x >> y;
+		list[x].pb(y);
+		list[y].pb(x);
+	}
+	bfs(list, n, 1);
+
+
+	return 0;
+}
