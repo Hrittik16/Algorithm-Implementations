ce72174eb7d77a12ff295df062be7cb8b964382c
hrittik16 <hrittikdas16@gmail.com>
2020-11-27 19:48:42 +0530

    updated

--- bfs.cpp
+++ bfs.cpp
@@ -1,43 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define vi vector<int>
-#define vl vector<ll>
-#define rep(i, a, b) for(int i = a; i < b; i++) 
-#define repll(i, a, b) for(ll i = a; i < b; i++) 
-#define all(a) a.begin(),a.end()
-#define pb push_back
 
-void bfs(vector<vector<int>>& list, int n, int beg) {
+vector<int> adj[100007];
+
+void bfs(int n, int s) {
 	queue<int> q;
-	vector<bool> check(n+1, 0);
-	vector<int> dist(n+1, 0); // bfs gives the shortest number of edges to go from beg(source) to any other node
-	q.push(beg);
-	check[beg] = 1;
-	while(!q.empty()) {
-		int curr = q.front();
+	vector<bool> visited(n + 1);
+	vector<int> p(n + 1), d(n + 1);
+
+	q.push(s);
+	visited[s] = true;
+	p[s] = -1;
+
+	while (!q.empty()) {
+		int u = q.front();
 		q.pop();
-		for(auto x: list[curr]) {
-			if(!check[x]) {
-				check[x] = 1;
+		for (auto x : adj[u]) {
+			if (!visited[x]) {
 				q.push(x);
-				dist[x] = dist[curr]+1;
+				visited[x] = true;
+				p[x] = u;
+				d[x] = d[u] + 1;
 			}
 		}
 	}
+
 }
 
 int main() {
+
 	int n, m;
 	cin >> n >> m;
-	vector<vector<int>> list(n+1); // adj list
-	rep(i, 0, m) {
+
+	for (int i = 0; i < m; i++) {
 		int x, y;
 		cin >> x >> y;
-		list[x].pb(y);
-		list[y].pb(x);
+		adj[x].push_back(y);
+		adj[y].push_back(x);
 	}
-	bfs(list, n, 1);
+
+	bfs(n, 1);
 
 
 	return 0;
